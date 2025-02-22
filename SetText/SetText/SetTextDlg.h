b84f45a598f7e9112fb9019db285b3a57b8fcbc8
jihu1130 <163612182+jihu1130@users.noreply.github.com>
2025-02-22 13:08:53 +0900

    Add files via upload

---
+++ SetText/SetText/SetTextDlg.h
@@ -0,0 +1,38 @@
+﻿
+// SetTextDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CSetTextDlg 대화 상자
+class CSetTextDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CSetTextDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SETTEXT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_ListBox;
+	afx_msg void OnSelchangeListBox();
+	afx_msg void OnClickedButtExit();
+};
